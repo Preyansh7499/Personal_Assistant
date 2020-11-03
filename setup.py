b4fb40a7d72ddd16e385c4c35e11f5a88e4b47e5
Preyansh Nagpal <preyansh7499@gmail.com>
2020-11-04 01:35:37 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(name = "pkg_hb",
-version = "1.0",
-description = "Personal Virtual Assistant",
-author = "Capt_Dazz",
-packages = ['pkg_hb'],
-install_requires = [])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pkg_hb",
+    version="0.0.1",
+    author="Preyansh Nagpal",
+    author_email="preyansh7499@gmail.com",
+    description="Voice Based Personal Assistant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Preyansh7499/Personal_Assistant",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
